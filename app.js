9438526c5eccbfe3cf7cb72643a4030c48d6af24
adomasr <adomas.razmus@gmail.com>
2023-07-25 03:04:50 +0100

    deploy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 const getMemesdata = async ()=> {
     try {
@@ -17,6 +17,23 @@ const getMemesdata = async ()=> {
         res.status(500).json({success: false, error: 'Error fetching memes data'});
     };
 };
+
+
+
+app.use(cors());
+app.use(express.static('public'));
+
+
+app.get('/api/memesData', async (req, res)=> {
+    const memesData = await getMemesdata();
+    res.status(200).json({success: true, data: memesData});
+});
+
+
+
+
+
+
 
 
 
@@ -46,16 +63,6 @@ const getMemesdata = async ()=> {
 //     }
 //   };
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-app.use(cors());
-app.use(express.static('public'));
-
-
-app.get('/api/memesData', async (req, res)=> {
-    const memesData = await getMemesdata();
-    res.status(200).json({success: true, data: memesData});
-});
-
-
 app.listen(port, ()=> {
     console.log('server is on port 5000');
 });
