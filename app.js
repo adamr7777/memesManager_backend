397c967d9e9195285a4129075b0a443f702e39fa
adomasr <adomas.razmus@gmail.com>
2023-08-22 01:38:11 +0100

    changes 9

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const cors = require('cors');
 require('dotenv').config();
 
 const mainRouter = require('./routes/main');
-const authRouter = require('./routes/auth');
-const authorize = require('./middleware/authorize');
 
 const app = express();
 const port = process.env.PORT || process.env.ALT_PORT;
@@ -14,8 +12,6 @@ const port = process.env.PORT || process.env.ALT_PORT;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/authorise', authRouter);
-app.use('/api/memesData', authorize);
 app.use('/api/memesData', mainRouter);
 
 
